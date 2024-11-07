a563c900e319f80459ead07db42ba3c3f913b8f8
Oumayma Sahmim <oumayma.sahmim@esprit.tn>
2024-11-07 23:58:53 +0100

    affichage-formations

--- src/app/apps/agrishare/formation/Components/formation/formation.component.ts
+++ src/app/apps/agrishare/formation/Components/formation/formation.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 import {ServiceblogService} from "../../../../blog/blog-service.service";
-
+import { FormationService } from '../../Services/formation.service';
+import { Component, OnInit } from '@angular/core'; 
+import { CommonModule } from "@angular/common";
 @Component({
   selector: 'app-formation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  
   templateUrl: './formation.component.html',
-  styleUrl: './formation.component.css'
+  styleUrls: ['./formation.component.css']
 })
-export class FormationComponent {
-  constructor(public service:ServiceblogService) {
+export class FormationComponent  implements OnInit  {
+  formations: any[] = [];
+  formation: any | null = null;
+  
+  constructor(
+  public service:ServiceblogService,
+  private formationService: FormationService,
+  private router: Router,
+  ) {
     this.service.showEdit=false;
-
   }
+
+  ngOnInit() {
+    // Récupérer toutes les formations lors de l'initialisation du composant
+    this.formationService.getAllFormations().subscribe(
+      (data) => {
+        console.log('Données récupérées :', data); // Afficher les données reçues
+        if (Array.isArray(data)) {
+          this.formations = data; // Assigner les formations récupérées si c'est un tableau
+        } else {
+          console.error('Les données récupérées ne sont pas un tableau');
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des formations', error);
+      }
+    );
+  }
+
 }
--- src/app/apps/agrishare/formation/Services/formation.service.ts
+++ src/app/apps/agrishare/formation/Services/formation.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Formation {
+  id: number;
+  titre: string;
+  description: string;
+  date: string;
+  type: string;
+  categorie: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormationService {
 
-  constructor() { }
+  private url: string  = 'http://localhost:8082/formations';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getAllFormations(): Observable<any> {
+    return this.http.get(this.url);
+}
+
+/*getAllFormations(): Observable<Formation[]> {
+  return this.http.get<Formation[]>(this.url);
+}*/
+
 }
