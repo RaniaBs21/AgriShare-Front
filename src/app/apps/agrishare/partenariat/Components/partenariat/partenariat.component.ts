cdbda5894b50f468dc386b9bde002376ac21fcdb
RaniaBs21 <raniabensalem53@gmail.com>
2024-11-10 22:15:35 +0100

    partenariat

--- src/app/apps/agrishare/partenariat/Components/partenariat/partenariat.component.ts
+++ src/app/apps/agrishare/partenariat/Components/partenariat/partenariat.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ServiceblogService} from "../../../../blog/blog-service.service";
+import {CommonModule} from "@angular/common";
+import {Partenariat} from "../../Models/Partenariat";
+import {PartenariatService} from "../../Services/partenariat.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-partenariat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './partenariat.component.html',
   styleUrl: './partenariat.component.css'
 })
-export class PartenariatComponent {
-  constructor(public service:ServiceblogService) {
-    this.service.showEdit=false;
+export class PartenariatComponent implements OnInit  {
+  partenariats: Partenariat[] = []; // Tableau de partenariats
+  loading: boolean = false; // Variable pour indiquer si les données sont en cours de chargement
 
+  constructor(private partenariatService: PartenariatService) {}
+
+  ngOnInit(): void {
+    this.getAllPartenariats(); // Appeler la méthode pour récupérer les partenariats au démarrage
+  }
+
+  // Méthode pour récupérer tous les partenariats
+  private getAllPartenariats(): void {
+    this.loading = true; // Activer le chargement pendant la récupération des données
+    this.partenariatService.getAllPartenariats().subscribe(
+      (response: Partenariat[]) => {
+        this.partenariats = response; // Assignation des partenariats à la variable
+        this.loading = false; // Désactivation du chargement après récupération
+      },
+      (error: HttpErrorResponse) => {
+        alert('Erreur lors de la récupération des partenariats: ' + error.message);
+        this.loading = false; // Désactivation du chargement en cas d'erreur
+      }
+    );
   }
 }
--- src/app/apps/agrishare/partenariat/Services/partenariat.service.ts
+++ src/app/apps/agrishare/partenariat/Services/partenariat.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Partenariat} from "../Models/Partenariat";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartenariatService {
 
-  constructor() { }
+  readonly API_URL = 'http://localhost:8096/partenariat';
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  // Ajouter un partenariat
+  addPartenariat(data: FormData): Observable<Partenariat> {
+    return this.httpClient.post<Partenariat>(this.API_URL, data);
+  }
+
+  // Récupérer tous les partenariats
+  getAllPartenariats(): Observable<Partenariat[]> {
+    return this.httpClient.get<Partenariat[]>(this.API_URL);
+  }
+
+  // Mettre à jour un partenariat
+  updatePartenariat(id: number, data: FormData): Observable<Partenariat> {
+    return this.httpClient.put<Partenariat>(`${this.API_URL}/${id}`, data);
+  }
+
+  // Supprimer un partenariat
+  deletePartenariat(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.API_URL}/${id}`);
+  }
+
+  // Trouver un partenariat par ID ou email
+  findPartenariatByIdOrEmail(id: number, email: string): Observable<Partenariat | null> {
+    return this.httpClient.get<Partenariat>(`${this.API_URL}/${id}/${email}`);
+  }
 }
