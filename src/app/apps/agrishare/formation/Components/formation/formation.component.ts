be7d95de92d7f167f863bf312aa48ef6b9aeee58
RaniaBs21 <raniabensalem53@gmail.com>
2024-11-11 00:09:35 +0100

    formation

--- src/app/apps/agrishare/formation/Components/formation/formation.component.ts
+++ src/app/apps/agrishare/formation/Components/formation/formation.component.ts
@@ -1,16 +1,157 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ServiceblogService} from "../../../../blog/blog-service.service";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormationService} from "../../Services/formation.service";
+import {Router} from "@angular/router";
+
+export enum TypeFormation {
+  GUIDE = 'Guide',
+  PRESENTIEL = 'Présentiel',
+  WEBINAIRE = 'Webinaire'
+}
+export enum CategorieFormation {
+  AGRICULTURE_BIO = 'Agriculture Bio',
+  GESTION_COOPERATIVE = 'Gestion de Coopérative',
+  MECANISATION = 'Mécanisation',
+  IRRIGATION = 'Irrigation',
+  COMMERCIALISATION = 'Commercialisation',
+  INNOVATION = 'Innovation'
+}
 
 @Component({
   selector: 'app-formation',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './formation.component.html',
-  styleUrl: './formation.component.css'
+  styleUrls: ['./formation.component.css']
 })
-export class FormationComponent {
-  constructor(public service:ServiceblogService) {
-    this.service.showEdit=false;
+export class FormationComponent implements OnInit {
+  formations: any[] = [];
+  formation: any = {
+    id: null, // Ajout de l'ID initialement à null
+    titre: '',
+    description: '',
+    date: '',
+    type: '',
+    categorie: ''
+  };
+  typeOptions = Object.keys(TypeFormation);
+  categorieOptions = Object.keys(CategorieFormation);
 
+  showFormEdit = false;
+  showForm = false;
+  initialFormation: any;
+
+
+  constructor(
+    public service: ServiceblogService,
+    private formationService: FormationService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.formationService.getAllFormations().subscribe(
+      (data) => {
+        if (Array.isArray(data)) {
+          this.formations = data;
+        } else {
+          console.error('Les données récupérées ne sont pas un tableau');
+        }
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des formations', error);
+      }
+    );
   }
+
+  participerFormation(formationId: number) {
+    console.log(`Participer à la formation avec l'ID: ${formationId}`);
+  }
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  toggleFormEdit(formationId: number): void {
+    const selectedFormation = this.formations.find(f => f.id === formationId);
+    if (selectedFormation) {
+      this.formation = { ...selectedFormation }; // Charger les données de la formation dans le formulaire
+      this.initialFormation = { ...selectedFormation };
+      this.showFormEdit = !this.showFormEdit; // Inverser l'état d'affichage
+      this.showForm = false; // Assurer que le formulaire d'ajout est masqué
+    }
+  }
+
+  ajouterFormation() {
+    this.formationService.addFormation(this.formation).subscribe(
+      (newFormation) => {
+        this.formations.push(newFormation);
+        this.showForm = false;
+        this.formation = { id: null, titre: '', description: '', date: '', type: '', categorie: '' }; // Réinitialisation complète
+      },
+      (error) => {
+        console.error('Erreur lors de l\'ajout de la formation', error);
+      }
+    );
+  }
+
+  loadFormationToEdit(formationId: number) {
+    const formationToEdit = this.formations.find(f => f.id === formationId);
+    if (formationToEdit) {
+      this.formation = { ...formationToEdit };  // Copie les données de la formation
+      this.showFormEdit = true;  // Affiche le formulaire d'édition
+    }
+  }
+
+  modifierFormation() {
+    if (this.formation.id) {
+      this.formationService.updateFormation(this.formation.id, this.formation).subscribe(
+        (response: string) => { // Modification pour recevoir une réponse de type string
+          console.log(response); // Message de succès ou d'erreur renvoyé par le serveur
+          const index = this.formations.findIndex(f => f.id === this.formation.id);
+          if (index !== -1) {
+            this.formations[index] = { ...this.formation }; // Mise à jour de la formation modifiée
+          }
+          this.showFormEdit = false;
+        },
+        (error) => {
+          console.error('Erreur lors de la modification de la formation', error);
+        }
+      );
+    }
+  }
+
+  deleteFormation(formationId: number): void {
+    this.formationService.deleteFormation(formationId).subscribe(
+      response => {
+        this.formations = this.formations.filter(f => f.id !== formationId);
+      },
+      error => {
+        console.error('Erreur lors de la suppression de la formation', error);
+      }
+    );
+  }
+
+  isFormValid(): boolean {
+    return this.formation.titre && this.formation.description && this.formation.date && this.formation.type && this.formation.categorie;
+  }
+
+  isFormEditModified(): boolean {
+    return JSON.stringify(this.formation) !== JSON.stringify(this.initialFormation);
+  }
+
+
+  cancelAjout(){
+    this.showForm = false;
+  }
+
+  cancelModif(){
+    this.showFormEdit = false;
+  }
+
 }
--- src/app/apps/agrishare/formation/Services/formation.service.ts
+++ src/app/apps/agrishare/formation/Services/formation.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import { catchError } from 'rxjs/operators';
 
+export interface Formation {
+  id: number;
+  titre: string;
+  description: string;
+  date: string;
+  type: string;
+  categorie: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class FormationService {
 
-  constructor() { }
+  private API_URL = 'http://localhost:8096/formation';
+
+   httpOptions = {
+     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+   };
+
+  constructor(private http: HttpClient) { }
+
+  getAllFormations(): Observable<any> {
+    return this.http.get(this.API_URL);
+  }
+
+  addFormation(formation: Formation): Observable<Formation> {
+    return this.http.post<Formation>(this.API_URL, formation, this.httpOptions);
+  }
+
+  // Méthode pour supprimer une formation
+  deleteFormation(id: number): Observable<string> {
+    return this.http.delete<string>(`${this.API_URL}/${id}`, { responseType: 'text' as 'json' }).pipe(
+      catchError(error => {
+        console.error('Erreur lors de la suppression de la formation', error);
+        throw error;
+      })
+    );
+  }
+
+  updateFormation(id: number, formation: Formation): Observable<string> {
+    return this.http.put<string>(`${this.API_URL}/${id}`, formation, {
+      headers: this.httpOptions.headers,
+      responseType: 'text' as 'json'
+    });
+  }
+
+
+
 }
