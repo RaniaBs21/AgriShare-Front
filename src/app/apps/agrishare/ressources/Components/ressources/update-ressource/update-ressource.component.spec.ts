78a3199805a0147bfc9e9d6d1f256d8bcad0bdd5
Nourkhalfaoui <nour.khalfaoui@esprit.tn>
2024-11-10 15:27:27 +0100

    update-ressource

---
+++ src/app/apps/agrishare/ressources/Components/ressources/update-ressource/update-ressource.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UpdateRessourceComponent } from './update-ressource.component';
+
+describe('UpdateRessourceComponent', () => {
+  let component: UpdateRessourceComponent;
+  let fixture: ComponentFixture<UpdateRessourceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UpdateRessourceComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UpdateRessourceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
